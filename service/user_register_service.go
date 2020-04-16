3ac85095bafadc9dc6c9c1f46d7295f205427539
NewtSun <1807118870@qq.com>
2020-04-16 22:30:18 +0800

    加入了rabbitmq异步处理oss文件上传

--- service/user_register_service.go
+++ service/user_register_service.go
@@ -8,30 +8,32 @@ import (
 // UserRegisterService 管理用户注册服务
 type UserRegisterService struct {
 	// Nickname        string `form:"nickname" json:"nickname" binding:"required,min=2,max=30"`
-	UserName string `form:"user_name" json:"user_name" binding:"required,min=5,max=30"`
-	Password string `form:"password" json:"password" binding:"required,min=8,max=40"`
-	// PasswordConfirm string `form:"password_confirm" json:"password_confirm" binding:"required,min=8,max=40"`
+	UserName        string `form:"user_name" json:"user_name" binding:"required,min=5,max=30"`
+	Password        string `form:"password" json:"password" binding:"required,min=8,max=40"`
+	PasswordConfirm string `form:"password_confirm" json:"password_confirm" binding:"required,min=8,max=40"`
+	Email           string `form:"email" json:"email"`
+	Phone           string `form:"phone" json:"phone"`
 }
 
 // valid 验证表单
 func (service *UserRegisterService) valid() *serializer.Response {
-	// if service.PasswordConfirm != service.Password {
-	// 	return &serializer.Response{
-	// 		Code: 40001,
-	// 		Msg:  "两次输入的密码不相同",
-	// 	}
-	// }
+	if service.PasswordConfirm != service.Password {
+		return &serializer.Response{
+			Code: 40001,
+			Msg:  "两次输入的密码不相同",
+		}
+	}
 
 	count := 0
-	// model.DB.Model(&model.User{}).Where("nickname = ?", service.Nickname).Count(&count)
-	// if count > 0 {
-	// 	return &serializer.Response{
-	// 		Code: 40001,
-	// 		Msg:  "昵称被占用",
-	// 	}
-	// }
+	model.DB.Model(&model.User{}).Where("user_name = ?", service.UserName).Count(&count)
+	if count > 0 {
+		return &serializer.Response{
+			Code: 40001,
+			Msg:  "昵称被占用",
+		}
+	}
 
-	// count = 0
+	count = 0
 	model.DB.Model(&model.User{}).Where("user_name = ?", service.UserName).Count(&count)
 	if count > 0 {
 		return &serializer.Response{
